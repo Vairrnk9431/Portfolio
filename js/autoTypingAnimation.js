017da64076fb92df033ef10ad9591eda62666149
Kumar Baibhav <121719532+Vairrnk9431@users.noreply.github.com>
2023-12-17 19:02:26 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Kumar Baibhav</span> an aspiring web developer fueled by passion and a commitment to crafting elegant and efficient code. As a recent graduate, I am excited to kickstart my journey in the dynamic world of web development."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
